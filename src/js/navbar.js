9628449aee2c21d05b7ac5f9e51c9cdad2ae9de8
EnricoSaggiorato <saggiorato.enrico@gmail.com>
2021-07-03 23:24:08 +0200

    sass structure

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,5 +2,6 @@ let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
 navBarToggle.addEventListener("click", function () {
+  console.log("TRIGGERED");
   mainNav.classList.toggle("active");
 });
