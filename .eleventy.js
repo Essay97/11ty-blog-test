bcd7239db4e733c9954948390448a505b43419e1
EnricoSaggiorato <saggiorato.enrico@gmail.com>
2021-07-06 08:57:05 +0200

    fixed conflicts

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addWatchTarget("./src/sass/");
 
+  eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
+
   return {
     dir: {
       input: "src",
