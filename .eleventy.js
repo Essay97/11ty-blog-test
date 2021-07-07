06084cb3dd362ff2d204ff81f12a597058a8abdd
EnricoSaggiorato <saggiorato.enrico@gmail.com>
2021-07-07 18:52:42 +0200

    restyled a bit, started media and about

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
 
   eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
+  eleventyConfig.addFilter("shift", (arr, shift) =>
+    arr.slice(shift, arr.length)
+  );
+  eleventyConfig.addFilter("debugJSON", (item) => {
+    console.log("JSON is: ", item);
+  });
 
   return {
     dir: {
